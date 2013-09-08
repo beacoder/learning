d49545318830496154b5457386219e0bc76b9925
chm <chenhuming@gmail.com>
2013-09-09 00:25:03 +0800

    add gmail account into gnus

--- Emacs/Gnus/.gnus.el
+++ Emacs/Gnus/.gnus.el
@@ -6,8 +6,22 @@
 ;; enable fetch email function
 ;; (setq gnus-secondary-select-methods '((nnml "")))
 
+;; add this to be able to list all labels in gmail
+(setq gnus-ignored-newsgroups "")
+
+;; to be able to search within your gmail/imap mail
+;; (require 'nnir)
+
+;; add this to configure gmail imap 
+(add-to-list 'gnus-secondary-select-methods '(nnimap "gmail"
+                                             (nnimap-address "imap.gmail.com")
+                                             (nnimap-server-port 993)
+                                             (nnimap-stream ssl)
+				             (nnir-search-engine imap)
+				             (nnimap-authinfo-file "~/.authinfo")))
+
 ;; set "nnml" method and pop server configuration 
-;; so that we could retrieve mails from pop-mail server
+;; so that we could retrieve mails from pop server
 
 (setq gnus-select-method '(nnml ""))
 
