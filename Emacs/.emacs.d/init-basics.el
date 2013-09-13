b0fc5cf3796ff27e9e8d4e4db51669fe3f67270e
陈虎明 <chenhuming@gmail.com>
2013-09-13 23:22:10 +0800

    Update init-basics.el

--- Emacs/.emacs.d/init-basics.el
+++ Emacs/.emacs.d/init-basics.el
@@ -1,1 +1,62 @@
-init-basics.
+
+;; enable narrow
+(put 'narrow-to-region 'disabled nil)
+
+;;----------------------------------------------------------------------------
+;; global key-bindings definition
+;;----------------------------------------------------------------------------
+
+;; bind the dired-jump to "C-x C-j"
+(global-set-key (kbd "C-x C-j") 'dired-jump)
+
+;;----------------------------------------------------------------------------
+;; mode setting
+;;----------------------------------------------------------------------------
+
+;; enable icomplete-mode
+(icomplete-mode t)
+
+;; enable ido-mode
+;;(ido-mode t)
+
+;; enable electric-pair-mode
+;;(electric-pair-mode t)
+
+;; save desktop
+;;(desktop-save-mode t)
+
+;; highlight the active region
+(transient-mark-mode t)
+
+;; hide toolbar
+(tool-bar-mode -1)
+
+;; hide scroll bar
+(scroll-bar-mode -1)
+
+;; enable the delete-selection-mode
+(delete-selection-mode t)
+
+;; enable global high light
+(global-font-lock-mode t)
+
+;; set text-mode as default major mode
+(setq default-major-mode 'text-mode)
+
+;; enable emacs to open image file
+(auto-image-file-mode)
+
+;;----------------------------------------------------------------------------
+;; some other settings
+;;----------------------------------------------------------------------------
+
+;; disable emacs system beep
+(setq visible-bell t)
+
+;; enlarge kill-ring-max value
+(setq kill-ring-max 200)
+
+;; show buffer-name in title
+(setq frame-title-format "emacs@%b")
+
+(provide 'init-basics)
