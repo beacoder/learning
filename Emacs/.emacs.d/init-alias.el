8c59fe9ac680cc7058c260d69233882b0eab4896
陈虎明 <chenhuming@gmail.com>
2013-09-13 23:06:28 +0800

    Update init-alias.el

--- Emacs/.emacs.d/init-alias.el
+++ Emacs/.emacs.d/init-alias.el
@@ -1,1 +1,23 @@
-init-alias.el
+;;----------------------------------------------------------------------------
+;; use alias to shorten commands
+;;----------------------------------------------------------------------------
+
+;; y or n is enough
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; always use ibuffer
+(defalias 'list-buffers 'ibuffer)
+
+;; sh stands for shell
+(defalias 'sh 'shell)
+
+;; eb stands for eval-buffer
+(defalias 'eb 'eval-buffer)
+
+;; elm stands for emacs-lisp-mode
+(defalias 'elm 'emacs-lisp-mode)
+
+;; ~ stands for make-backup
+(defalias '~ 'make-backup)
+
+(provide 'init-alias)
