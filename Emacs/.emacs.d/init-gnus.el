1c5274bb7a060e3623bdd644310374e5f8413799
陈虎明 <chenhuming@gmail.com>
2013-09-13 22:45:43 +0800

    Update init-gnus.el

--- Emacs/.emacs.d/init-gnus.el
+++ Emacs/.emacs.d/init-gnus.el
@@ -37,7 +37,7 @@
                                              (nnimap-server-port 993)
                                              (nnimap-stream ssl)
 				             ;; (nnir-search-engine imap)
-				             (nnimap-authinfo-file "~/.authinfo")))
+				             (nnimap-authinfo-file "~/.emacs.d/.authinfo")))
 
 ;; send mails using huatek's smtp server
 ;; (setq smtpmail-auth-credentials
