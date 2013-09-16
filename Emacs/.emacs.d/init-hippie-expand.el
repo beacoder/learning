07d334b3e233c4e7e41cb795583b0f9226db9791
陈虎明 <chenhuming@gmail.com>
2013-09-16 12:09:45 +0800

    Update init-hippie-expand.el

--- Emacs/.emacs.d/init-hippie-expand.el
+++ Emacs/.emacs.d/init-hippie-expand.el
@@ -49,6 +49,9 @@
 (setq hippie-expand-try-functions-list
       '(try-complete-file-name-partially
         try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list
+        try-expand-line
         try-expand-dabbrev
         try-expand-dabbrev-all-buffers
         try-expand-dabbrev-from-kill
