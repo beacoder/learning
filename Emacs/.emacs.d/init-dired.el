e422d46b311ea8f54ebd01ef1b6448cbaeb6ca1f
陈虎明 <chenhuming@gmail.com>
2013-09-23 11:00:45 +0800

    Update init-dired.el

--- Emacs/.emacs.d/init-dired.el
+++ Emacs/.emacs.d/init-dired.el
@@ -6,7 +6,7 @@
 (global-set-key (kbd "C-x C-j") 'dired-jump)
 
 ;; allow dired to be able to delete or copy a whole dir.
-;; “always” means no asking. “top” means ask once. 
+;; "always" means no asking. "top" means ask once. 
 ;; Any other symbol means ask each and every time for a dir and subdir.
 (setq dired-recursive-copies (quote always))
 (setq dired-recursive-deletes (quote top))
