7a3633f311ef9735aaaa625f8078fbfa6788cb59
陈虎明 <chenhuming@gmail.com>
2013-10-23 09:49:04 +0800

    Create init-macros.el

---
+++ Emacs/.emacs.d/init-macros.el
@@ -0,0 +1,7 @@
+;; save kbd-macros definitions
+
+;; restore last workspace
+(fset 'restore-workspace
+   [?\C-x ?2 ?\C-x ?3 ?\M-x ?s ?h return ?\C-x ?o ?\C-x ?d return ?\C-x ?o ?\C-x ?d return ?\C-x ?o ?\C-x ?r ?w ?w])
+
+(provide 'init-macros)
