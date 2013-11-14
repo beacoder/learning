bd67c53b7747d72f380b8a0a3398e5f8572d50d7
陈虎明 <chenhuming@gmail.com>
2013-11-14 13:01:08 +0800

    Update init-cc-mode.el

--- Emacs/.emacs.d/init-cc-mode.el
+++ Emacs/.emacs.d/init-cc-mode.el
@@ -45,28 +45,18 @@
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
 
 ;; show function name in mode-line
-(add-hook 'c-mode-common-hook
-  (lambda()
-    (which-function-mode t)))
+(add-hook 'c-mode-common-hook (lambda() (which-function-mode t)))
 
 ;; navigation between header and cpp/cc files
-(add-hook 'c-mode-common-hook
-  (lambda()
-    (local-set-key (kbd "C-c o") 'ff-find-other-file)))
+(add-hook 'c-mode-common-hook (lambda() (local-set-key (kbd "C-c o") 'ff-find-other-file)))
     
 ;; jump to the start of the function    
-(add-hook 'c-mode-common-hook
-  (lambda()
-    (local-set-key (kbd "C-M-a") 'c-beginning-of-defun)))
+(add-hook 'c-mode-common-hook (lambda() (local-set-key (kbd "C-M-a") 'c-beginning-of-defun)))
 
 ;; jump to the end of the function    
-(add-hook 'c-mode-common-hook
-  (lambda()
-    (local-set-key (kbd "C-M-e") 'c-end-of-defun)))
+(add-hook 'c-mode-common-hook (lambda() (local-set-key (kbd "C-M-e") 'c-end-of-defun)))
     
 ;; use <f5> to compile
-(add-hook 'c-mode-common-hook
-  (lambda()
-    (local-set-key (kbd "<f5>") 'compile)))
+(add-hook 'c-mode-common-hook (lambda() (local-set-key (kbd "<f5>") 'compile)))
 
 (provide 'init-cc-mode)
