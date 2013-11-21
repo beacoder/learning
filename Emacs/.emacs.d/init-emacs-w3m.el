b3a85f606da2dfc0b6454cc36cc94e89fae52bbb
陈虎明 <chenhuming@gmail.com>
2013-11-21 21:50:09 +0800

    Update init-emacs-w3m.el

--- Emacs/.emacs.d/init-emacs-w3m.el
+++ Emacs/.emacs.d/init-emacs-w3m.el
@@ -10,32 +10,28 @@
       w3m-input-coding-system 'utf-8
       w3m-output-coding-system 'utf-8
       w3m-terminal-coding-system 'utf-8)
-(setq w3m-use-cookies t)
-(setq w3m-cookie-accept-bad-cookies t)
-(setq w3m-home-page
-      (if (file-readable-p "~/html/home.html")
-	  (concat "file://" (expand-file-name "~/html/home.html"))
-        "http://www.google.com.au"))
-
-(setq w3m-use-toolbar t
-      ;w3m-use-tab     nil
-      w3m-key-binding 'info
-      )
+      
+(setq w3m-use-cookies t
+      w3m-cookie-accept-bad-cookies t
+      w3m-home-page "http://www.google.com"
+      w3m-use-toolbar t
+      ;; w3m-use-tab     nil
+      w3m-key-binding 'info)
 
 ;; show images in the browser
-;(setq w3m-default-display-inline-images t)
+;; (setq w3m-default-display-inline-images t)
 
 (setq w3m-search-default-engine "g")
 (eval-after-load "w3m-search" '(progn
-                                 ; C-u S g RET <search term> RET
-                                 (add-to-list 'w3m-search-engine-alist '("g" "http://www.google.com.au/search?hl=en&q=%s" utf-8))
+                                 ;; C-u S g RET <search term> RET
+                                 (add-to-list 'w3m-search-engine-alist '("g" "http://www.google.com/search?hl=en&q=%s" utf-8))
                                  (add-to-list 'w3m-search-engine-alist '("wz" "http://zh.wikipedia.org/wiki/Special:Search?search=%s" utf-8))
-                                 (add-to-list 'w3m-search-engine-alist '("q" "http://www.google.com.au/search?hl=en&q=%s+site:stackoverflow.com" utf-8))
+                                 (add-to-list 'w3m-search-engine-alist '("q" "http://www.google.com/search?hl=en&q=%s+site:stackoverflow.com" utf-8))
                                  (add-to-list 'w3m-search-engine-alist '("s" "http://code.ohloh.net/search?s=%s&browser=Default"  utf-8))
-                                 (add-to-list 'w3m-search-engine-alist '("b" "http://blogsearch.google.com.au/blogsearch?q=%s" utf-8))
+                                 (add-to-list 'w3m-search-engine-alist '("b" "http://blogsearch.google.com/blogsearch?q=%s" utf-8))
                                  (add-to-list 'w3m-search-engine-alist '("w" "http://en.wikipedia.org/wiki/Special:Search?search=%s" utf-8))
                                  (add-to-list 'w3m-search-engine-alist '("d" "http://dictionary.reference.com/search?q=%s" utf-8))
-                                 (add-to-list 'w3m-search-engine-alist '("j" "http://www.google.com.au/search?ie=UTF-8&oe=UTF-8&sourceid=navclient&btnI=1&q=%s+site:developer.mozilla.org" utf-8))
+                                 (add-to-list 'w3m-search-engine-alist '("j" "http://www.google.com/search?ie=UTF-8&oe=UTF-8&sourceid=navclient&btnI=1&q=%s+site:developer.mozilla.org" utf-8))
                                  ))
 
 (setq w3m-command-arguments       '("-F" "-cookie")
@@ -43,13 +39,13 @@
       browse-url-browser-function 'w3m
       mm-text-html-renderer       'w3m)
 
-;bind this function to a, which is the normal w3m bookmark binding:
+;; bind this function to A, which is the normal w3m bookmark binding:
 (eval-after-load "w3m" '(progn
                           (define-key w3m-info-like-map "A" 'delicious-post)
                           ;; (w3m-lnum-mode 1)
                           ))
 
-; external browser
+;; external browser
 (setq browse-url-generic-program
       (executable-find "firefox"))
 
@@ -60,13 +56,13 @@
   (require 'w3m)
   (let ((keyword (w3m-url-encode-string (thing-at-point 'symbol))))
     ;; google
-    (browse-url-generic (concat "http://www.google.com.au/search?hl=en&q=%22"
+    (browse-url-generic (concat "http://www.google.com/search?hl=en&q=%22"
                                 keyword
                                 "%22"
                                 (if buffer-file-name
 				    (concat "+filetype%3A" (file-name-extension buffer-file-name))
 				  "")  ))
-    (browse-url-generic (concat "http://www.google.com.au/search?hl=en&q="
+    (browse-url-generic (concat "http://www.google.com/search?hl=en&q="
                                 keyword
                                 "+site:stackoverflow.com" ))
     ;; koders.com
