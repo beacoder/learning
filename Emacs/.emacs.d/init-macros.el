873b790a47ab579710fcbdf826f943490608d0b1
陈虎明 <chenhuming@gmail.com>
2013-11-23 11:36:10 +0800

    Update init-macros.el

--- Emacs/.emacs.d/init-macros.el
+++ Emacs/.emacs.d/init-macros.el
@@ -4,4 +4,32 @@
 (fset 'restore-workspace
    [?\C-x ?2 ?\C-x ?3 ?\M-x ?s ?h return ?\C-x ?o ?\C-x ?d return ?\C-x ?o ?\C-x ?d return ?\C-x ?o ?\C-x ?r ?w ?w])
 
+;; apply function to region lines inspired by "apply-macro-to-region-lines"
+(defun apply-function-to-region-lines (fn)
+  (interactive "aFunction to apply to lines in region: ")
+  (save-excursion
+    (goto-char (region-end))
+    (let ((end-marker (copy-marker (point-marker)))
+          next-line-marker)
+      (goto-char (region-beginning))
+      (if (not (bolp))
+          (forward-line 1))
+      (setq next-line-marker (point-marker))
+      (while (< next-line-marker end-marker)
+        (let ((start nil)
+              (end nil))
+          (goto-char next-line-marker)
+          (save-excursion
+            (setq start (point))
+            (forward-line 1)
+            (set-marker next-line-marker (point))
+            (setq end (point)))
+          (save-excursion
+            (let ((mark-active nil))
+              (narrow-to-region start end)
+              (funcall fn)
+              (widen)))))
+      (set-marker end-marker nil)
+      (set-marker next-line-marker nil))))   
+
 (provide 'init-macros)
