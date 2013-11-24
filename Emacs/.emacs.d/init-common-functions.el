d29dbbba482d97a4cc61f0c7c9229a1227fdfbf7
陈虎明 <chenhuming@gmail.com>
2013-11-24 11:42:22 +0800

    Create init-common-functions.el

---
+++ Emacs/.emacs.d/init-common-functions.el
@@ -0,0 +1,25 @@
+;; define some common functions
+
+;; trim string
+(defun trim-string (string)
+  "Remove white spaces in beginning and ending of STRING.
+White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
+  (replace-regexp-in-string "\\`[ \t\n]*" "" (replace-regexp-in-string "[ \t\n]*\\'" "" string))
+  )
+  
+;; get line contents as a string
+(defun getline ()
+  "Get line contents as a string."
+  (let (beg end line-string)
+    (setq beg (line-beginning-position))
+    (setq end (line-end-position))
+    (setq line-string (buffer-substring-no-properties beg end))
+    )
+  )
+
+(defun getline-trimmed ()
+  "Get line contents as a trimmed string."
+  (trim-string (getline))
+  )
+
+(provide 'init-common-functions)
