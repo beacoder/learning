56ea6848237404d20e3b80e61df99851056b9a03
陈虎明 <chenhuming@gmail.com>
2013-11-24 12:49:39 +0800

    Update init-com-functions.el

--- Emacs/.emacs.d/init-com-functions.el
+++ Emacs/.emacs.d/init-com-functions.el
@@ -24,4 +24,33 @@ White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
   "Return line contents as a string and trim it."
   (trim-string (getline)))
 
+;; Inspired by apply-macro-to-region-lines
+(defun apply-function-to-region-lines (fn)
+  "Apply function to region lines."
+  (interactive "aFunction to apply to lines in region: ")
+  (save-excursion
+    (goto-char (region-end))
+    (let ((end-marker (copy-marker (point-marker)))
+          next-line-marker)
+      (goto-char (region-beginning))
+      (if (not (bolp))
+          (forward-line 1))
+      (setq next-line-marker (point-marker))
+      (while (< next-line-marker end-marker)
+        (let ((start nil)
+              (end nil))
+          (goto-char next-line-marker)
+          (save-excursion
+            (setq start (point))
+            (forward-line 1)
+            (set-marker next-line-marker (point))
+            (setq end (point)))
+          (save-excursion
+            (let ((mark-active nil))
+              (narrow-to-region start end)
+              (funcall fn)
+              (widen)))))
+      (set-marker end-marker nil)
+      (set-marker next-line-marker nil))))
+
 (provide 'init-com-functions)
