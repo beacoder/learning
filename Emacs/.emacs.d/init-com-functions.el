75d9530f1421690eaf5fd0b1f357f837683f8136
陈虎明 <chenhuming@gmail.com>
2013-11-24 12:14:00 +0800

    Update init-com-functions.el

--- Emacs/.emacs.d/init-com-functions.el
+++ Emacs/.emacs.d/init-com-functions.el
@@ -6,20 +6,22 @@
 White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
   (replace-regexp-in-string "\\`[ \t\n]*" "" (replace-regexp-in-string "[ \t\n]*\\'" "" string))
   )
-  
-;; get line contents as a string
+
 (defun getline ()
   "Get line contents as a string."
   (let (beg end line-string)
     (setq beg (line-beginning-position))
     (setq end (line-end-position))
-    (setq line-string (buffer-substring-no-properties beg end))
-    )
-  )
-
+    (setq line-string (buffer-substring-no-properties beg end))))
+    
+(defun getline-nth (line-number)
+  "Get specific line's contents as a string."
+  (save-excursion
+    (goto-line line-number)
+    (getline)))
+  
 (defun getline-trim ()
   "Get line contents as a string and trim it."
-  (trim-string (getline))
-  )
+  (trim-string (getline)))
 
 (provide 'init-com-functions)
