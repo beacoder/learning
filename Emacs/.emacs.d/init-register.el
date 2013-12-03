8bac1233d68196c146c002d2255004f73554ac4e
陈虎明 <chenhuming@gmail.com>
2013-12-03 20:12:19 +0800

    Update init-register.el

--- Emacs/.emacs.d/init-register.el
+++ Emacs/.emacs.d/init-register.el
@@ -3,4 +3,7 @@
 ;; "C-x r j e" to open file ".emacs" 
 (set-register ?e '(file . "~/.emacs"))
 
+;; "C-x r j h" to open file ".bash_history" 
+(set-register ?h '(file . "~/.bash_history"))
+
 (provide 'init-register)
