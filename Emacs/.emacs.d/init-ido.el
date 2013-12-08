5f8b292c575298c1bc11e64ea3a12ab2795fd917
陈虎明 <chenhuming@gmail.com>
2013-12-08 22:08:56 +0800

    Create init-ido.el

---
+++ Emacs/.emacs.d/init-ido.el
@@ -0,0 +1,12 @@
+
+(defun ido-find-file-in-tag-files ()
+      (interactive)
+      (save-excursion
+        (let ((enable-recursive-minibuffers t))
+          (visit-tags-table-buffer))
+        (find-file
+         (expand-file-name
+          (ido-completing-read
+           "Project file: " (tags-table-files) nil t)))))
+           
+(provide 'init-ido)
