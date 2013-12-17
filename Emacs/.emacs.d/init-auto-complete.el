313e7d15b2e6f971331ea290639fa4a9d1f5fbeb
陈虎明 <chenhuming@gmail.com>
2013-12-17 22:11:53 +0800

    Update init-auto-complete.el

--- Emacs/.emacs.d/init-auto-complete.el
+++ Emacs/.emacs.d/init-auto-complete.el
@@ -11,3 +11,27 @@
     (require 'auto-complete-config)
     (ac-config-default)
     (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")))
+    
+;; enable pos-tip
+(require 'pos-tip)
+(setq ac-quick-help-prefer-pos-tip t)
+
+;; show tooltip
+(setq ac-use-quick-help t)
+(setq ac-quick-help-delay 1.0)
+
+(setq ac-dwim t)
+
+;; auto complete using clang is CPU sensitive
+(setq ac-auto-start nil)
+(ac-set-trigger-key "<C-return>")
+
+;; add function into ac-trigger-commands to trigger complete
+(setq ac-trigger-commands
+   (cons 'backward-delete-char-untabify ac-trigger-commands))
+   
+;; trigger complete even if your typing is wrong   
+(setq ac-fuzzy-enable t)
+
+(provide 'init-auto-complete)
+
