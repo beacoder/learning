b2019c16c3951854dfd54efeb9874c6c904c0b3c
陈虎明 <chenhuming@gmail.com>
2013-12-17 22:16:54 +0800

    Update init-auto-complete.el

--- Emacs/.emacs.d/init-auto-complete.el
+++ Emacs/.emacs.d/init-auto-complete.el
@@ -16,6 +16,26 @@
 (require 'pos-tip)
 (setq ac-quick-help-prefer-pos-tip t)
 
+;; show quick help of popup.el by real tooltip
+(defadvice popup-menu-show-quick-help
+  (around pos-tip-popup-menu-show-quick-help () activate)
+  "Show quick help using `pos-tip-show'."
+  (if (eq window-system 'x)
+      (let ((doc (popup-menu-document
+		  menu (or item
+			   (popup-selected-item menu)))))
+	(when (stringp doc)
+	  (pos-tip-show doc nil
+			(if (popup-hidden-p menu)
+			    (or (plist-get args :point)
+				(point))
+			  (overlay-end (popup-line-overlay
+					menu (+ (popup-offset menu)
+						(popup-selected-line menu)))))
+			nil 0)
+	  nil))
+    ad-do-it))
+
 ;; show tooltip
 (setq ac-use-quick-help t)
 (setq ac-quick-help-delay 1.0)
