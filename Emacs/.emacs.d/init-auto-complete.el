35e22bfbc8a461f1926ed60d5041b4f207eda798
陈虎明 <chenhuming@gmail.com>
2013-12-17 23:07:07 +0800

    Update init-auto-complete.el

--- Emacs/.emacs.d/init-auto-complete.el
+++ Emacs/.emacs.d/init-auto-complete.el
@@ -7,6 +7,7 @@
 ;;----------------------------------------------------------------------------
 
 ;; basic auto-complete configuration
+(require 'auto-complete)
 (require 'auto-complete-config)
 (ac-config-default)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
