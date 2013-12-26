ee6931ecfa7e6fa57a6ecd0a6672eef36bf243c5
陈虎明 <chenhuming@gmail.com>
2013-12-26 12:18:17 +0800

    Update init-lua.el

--- Emacs/.emacs.d/init-lua.el
+++ Emacs/.emacs.d/init-lua.el
@@ -6,3 +6,15 @@
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(add-hook 'lua-mode-hook
+          (lambda ()
+            (setq safe-local-variable-values
+                  '((lua-indent-level . 2)
+                    (lua-indent-level . 3)
+                    (lua-indent-level . 4)
+                    (lua-indent-level . 8)))
+            (flymake-lua-load)
+            ))
+
+(provide 'init-lua)
