e841c280cc23c7cc0b839526831670613a01f947
陈虎明 <chenhuming@gmail.com>
2014-02-21 15:40:36 +0800

    Update init-basics.el

--- Emacs/.emacs.d/init-basics.el
+++ Emacs/.emacs.d/init-basics.el
@@ -152,27 +152,4 @@
 (require 'saveplace)
 (setq-default save-place t)
 
-;; freeze a specific buffer
-;; begin
-(defadvice pop-to-buffer (before cancel-other-window first)
-  (ad-set-arg 1 nil))
-
-(ad-activate 'pop-to-buffer)
-
-;; Toggle window dedication
-(defun toggle-window-dedicated ()
-  "Toggle whether the current active window is dedicated or not"
-  (interactive)
-  (message
-   (if (let (window (get-buffer-window (current-buffer)))
-         (set-window-dedicated-p window 
-                                 (not (window-dedicated-p window))))
-       "Window '%s' is dedicated"
-     "Window '%s' is normal")
-   (current-buffer)))
-
-;; Press [pause] key in each window you want to "freeze"
-(global-set-key [pause] 'toggle-window-dedicated)
-;; end
-
 (provide 'init-basics)
