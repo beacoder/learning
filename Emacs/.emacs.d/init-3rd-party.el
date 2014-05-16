4f40721c28a9486fbb8113536a5cfb76674728b8
陈虎明 <chenhuming@gmail.com>
2014-05-16 09:50:19 +0800

    Create init-3rd-party.el

---
+++ Emacs/.emacs.d/init-3rd-party.el
@@ -0,0 +1,32 @@
+;;----------------------------------------------------------------------------
+;; multiple-cursors setting
+;;----------------------------------------------------------------------------
+
+;; add to load-path
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elpa/multiple-cursors-20140418.815"))
+
+(require 'multiple-cursors)
+
+;;----------------------------------------------------------------------------
+;; auto-complete setting
+;;----------------------------------------------------------------------------
+
+;; add to load-path
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elpa/popup-20140207.1702"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elpa/auto-complete-20140512.43"))
+
+;; do default config for auto-complete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20140512.43/dict")
+(ac-config-default)
+
+;; auto complete is CPU sensitive
+(ac-set-trigger-key "<C-return>")
+
+;; use tooltip and set delay time
+(setq ac-auto-start nil
+      ac-use-quick-help t
+      ac-quick-help-delay 0.5
+      ac-quick-help-height 30)
+
+(provide 'init-3rd-party)
