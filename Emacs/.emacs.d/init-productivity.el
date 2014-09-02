55c4a52a90ffc06487ebecb2f273ec9fa9ddb58b
陈虎明 <chenhuming@gmail.com>
2014-09-02 18:20:12 +0800

    Update init-productivity.el

--- Emacs/.emacs.d/init-productivity.el
+++ Emacs/.emacs.d/init-productivity.el
@@ -25,7 +25,7 @@ This command is similar to `find-file-at-point' but without prompting for confir
             (when (y-or-n-p (format "file doesn't exist: 「%s」. Create?" path) )
               (find-file path )) ) ) ) ) ))
 
-;; try to make key-bindings follow the "action-where-object" pattern,
+;; try to follow the "action-where-object" pattern when defining key-bindings,
 ;; and use as less keys as possible.
 
 ;; http://ergoemacs.org/emacs/elisp_modify_syntax_table_temporarily.html
@@ -53,22 +53,76 @@ This command is similar to `find-file-at-point' but without prompting for confir
       (goto-char (1+ p1))
       (set-mark (1- p2)))))
 
-;; "d b" => delete text block.
-(global-set-key (kbd "C-c d b") 'xah-delete-text-block)
-(defun xah-delete-text-block ()
-  "delete the current text block (paragraph) and also put it to `kill-ring'."
+;; "d b" => delete text inside bracket.
+(global-set-key (kbd "C-c d b") 'delete-text-in-bracket)
+(defun delete-text-in-bracket ()
+  "Delete text between the nearest brackets.
+⁖  () [] {} «» ‹› “” 〖〗 【】 「」 『』 （） 〈〉 《》 〔〕 ⦗⦘ 〘〙 ⦅⦆ 〚〛 ⦃⦄ ⟨⟩."
+  (interactive)
+  (xah-select-text-in-bracket)
+  (if (region-active-p)
+      (kill-region (region-beginning) (region-end))))
+
+;; "a p" => select a text paragraph (block).
+(global-set-key (kbd "C-c a p") 'xah-select-current-block)
+(defun xah-select-current-block ()
+  "Select the current block of text between blank lines."
   (interactive)
   (let (p1 p2)
     (progn
-      (if (re-search-backward "\n[ \t]*\n" nil "NOERROR")
+      (if (re-search-backward "\n[ \t]*\n" nil "move")
           (progn (re-search-forward "\n[ \t]*\n")
                  (setq p1 (point)))
         (setq p1 (point)))
-      (if (re-search-forward "\n[ \t]*\n" nil "NOERROR")
+      (if (re-search-forward "\n[ \t]*\n" nil "move")
           (progn (re-search-backward "\n[ \t]*\n")
-                 (setq p2 (point) ))
-        (setq p2 (point))) )
-    (kill-region p1 p2)
-    (delete-blank-lines) ))
+                 (setq p2 (point)))
+        (setq p2 (point))))
+    (set-mark p1)))
+
+;; "d p" => delete text paragraph (block).
+(global-set-key (kbd "C-c d p") 'xah-delete-text-block)
+(defun xah-delete-text-block ()
+  "Delete the current text block (paragraph) and also put it to `kill-ring'."
+  (interactive)
+  (xah-select-current-block)
+  (if (region-active-p)
+      (progn (kill-region (region-beginning) (region-end))
+             (delete-blank-lines))))
+
+;; by Nikolaj Schumacher, 2008-10-20. Released under GPL.
+(global-set-key (kbd "M-8") 'extend-selection)
+(defun semnav-up (arg)
+  (interactive "p")
+  (when (nth 3 (syntax-ppss))
+    (if (> arg 0)
+        (progn
+          (skip-syntax-forward "^\"")
+          (goto-char (1+ (point)))
+          (decf arg))
+      (skip-syntax-backward "^\"")
+      (goto-char (1- (point)))
+      (incf arg)))
+  (up-list arg))
+
+;; by Nikolaj Schumacher, 2008-10-20. Released under GPL.
+(defun extend-selection (arg &optional incremental)
+  "Select the current word.
+Subsequent calls expands the selection to larger semantic unit."
+  (interactive (list (prefix-numeric-value current-prefix-arg)
+                     (or (use-region-p)
+                         (eq last-command this-command))))
+  (if incremental
+      (progn
+        (semnav-up (- arg))
+        (forward-sexp)
+        (mark-sexp -1))
+    (if (> arg 1)
+        (extend-selection (1- arg) t)
+      (if (looking-at "\\=\\(\\s_\\|\\sw\\)*\\_>")
+          (goto-char (match-end 0))
+        (unless (memq (char-before) '(?\) ?\"))
+          (forward-sexp)))
+      (mark-sexp -1))))
 
 (provide 'init-productivity)
