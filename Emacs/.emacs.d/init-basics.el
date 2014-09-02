9267aabba12b4ea4ea9d5582d1eb9aa794034d09
陈虎明 <chenhuming@gmail.com>
2014-09-02 10:40:48 +0800

    Update init-basics.el

--- Emacs/.emacs.d/init-basics.el
+++ Emacs/.emacs.d/init-basics.el
@@ -7,31 +7,6 @@
 ;; In term-char-mode, or when there is no Meta key.
 (global-set-key (kbd "C-x C-m") 'execute-extended-command)
 
-;; bind Open File Path Under Cursor Fast
-(global-set-key (kbd "C-c j") 'open-file-at-cursor)
-
-;; @see http://ergoemacs.org/emacs/emacs_open_file_path_fast.html
-(defun open-file-at-cursor ()
-  "Open the file path under cursor.
-If there is text selection, uses the text selection for path.
-If the path is starts with “http://”, open the URL in browser.
-Input path can be {relative, full path, URL}.
-This command is similar to `find-file-at-point' but without prompting for confirmation.
-"
-  (interactive)
-  (let ( (path (if (region-active-p)
-                   (buffer-substring-no-properties (region-beginning) (region-end))
-                 (thing-at-point 'filename) ) ))
-    (if (string-match-p "\\`https?://" path)
-        (browse-url path)
-      (progn ; not starting “http://”
-        (if (file-exists-p path)
-            (find-file path)
-          (if (file-exists-p (concat path ".el"))
-              (find-file (concat path ".el"))
-            (when (y-or-n-p (format "file doesn't exist: 「%s」. Create?" path) )
-              (find-file path )) ) ) ) ) ))
-
 ;; bind pop-tag-mark
 (global-set-key (kbd "M-?") 'pop-tag-mark)
 
