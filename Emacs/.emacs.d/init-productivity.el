65a80695aa5ef6a4104e1e2b3e16dfc69233c865
陈虎明 <chenhuming@gmail.com>
2014-09-02 10:42:05 +0800

    Create init-productivity.el

---
+++ Emacs/.emacs.d/init-productivity.el
@@ -0,0 +1,54 @@
+;;----------------------------------------------------------------------------
+;; utilities for productivity
+;;----------------------------------------------------------------------------
+
+;; bind Open File Path Under Cursor Fast
+(global-set-key (kbd "C-c j") 'open-file-at-cursor)
+
+;; @see http://ergoemacs.org/emacs/emacs_open_file_path_fast.html
+(defun open-file-at-cursor ()
+  "Open the file path under cursor.
+If there is text selection, uses the text selection for path.
+If the path is starts with “http://”, open the URL in browser.
+Input path can be {relative, full path, URL}.
+This command is similar to `find-file-at-point' but without prompting for confirmation.
+"
+  (interactive)
+  (let ( (path (if (region-active-p)
+                   (buffer-substring-no-properties (region-beginning) (region-end))
+                 (thing-at-point 'filename) ) ))
+    (if (string-match-p "\\`https?://" path)
+        (browse-url path)
+      (progn ; not starting “http://”
+        (if (file-exists-p path)
+            (find-file path)
+          (if (file-exists-p (concat path ".el"))
+              (find-file (concat path ".el"))
+            (when (y-or-n-p (format "file doesn't exist: 「%s」. Create?" path) )
+              (find-file path )) ) ) ) ) ))
+
+;; http://ergoemacs.org/emacs/elisp_modify_syntax_table_temporarily.html
+(global-set-key (kbd "C-c i b") 'xah-select-text-in-bracket)
+(defun xah-select-text-in-bracket ()
+  "Select text between the nearest brackets.
+⁖  () [] {} «» ‹› “” 〖〗 【】 「」 『』 （） 〈〉 《》 〔〕 ⦗⦘ 〘〙 ⦅⦆ 〚〛 ⦃⦄ ⟨⟩."
+  (interactive)
+  (with-syntax-table (standard-syntax-table)
+    (modify-syntax-entry ?\« "(»")
+    (modify-syntax-entry ?\» ")«")
+    (modify-syntax-entry ?\‹ "(›")
+    (modify-syntax-entry ?\› ")‹")
+    (modify-syntax-entry ?\“ "(”")
+    (modify-syntax-entry ?\” ")“")
+    (modify-syntax-entry ?\‘ "(’")
+    (modify-syntax-entry ?\’ ")‘")
+    (let (pos p1 p2)
+      (setq pos (point))
+      (search-backward-regexp "\\s(" nil t )
+      (setq p1 (point))
+      (forward-sexp 1)
+      (setq p2 (point))
+      (goto-char (1+ p1))
+      (set-mark (1- p2)))))
+
+(provide 'init-productivity)
