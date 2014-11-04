648a141aa15cb3066ee0e5530cbc2ca412641cdf
陈虎明 <chenhuming@gmail.com>
2014-11-04 09:32:31 +0800

    Update init-highline.el

--- Emacs/.emacs.d/elisp/init-highline.el
+++ Emacs/.emacs.d/elisp/init-highline.el
@@ -5,16 +5,17 @@
 
 (defun highline-mode-on () (highline-mode 1))
 
-;; Turn on local highlighting for Dired (C-x d)
-(add-hook 'dired-after-readin-hook #'highline-mode-on)
-
 ;; Turn on local highlighting for list-buffers (C-x C-b)
 ;; (defadvice list-buffers (after highlight-line activate)
 ;;   (save-excursion
 ;;     (set-buffer "*Buffer List*")
 ;;     (highline-mode-on)))
 
-;; Turn on local highlighting for c/c++ files    
-(add-hook 'c-mode-common-hook 'highline-mode-on)
+(when (display-graphic-p)
+  ;; Turn on local highlighting for Dired (C-x d)
+  (add-hook 'dired-after-readin-hook #'highline-mode-on)
+
+  ;; Turn on local highlighting for c/c++ files
+  (add-hook 'c-mode-common-hook 'highline-mode-on))
 
 (provide 'init-highline)
