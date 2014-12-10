fd7711f4f8082dfbad41630f28571803b650eed8
陈虎明 <chenhuming@gmail.com>
2014-12-10 11:38:13 +0800

    Create init-ido.el

---
+++ Emacs/.emacs.d/elisp/init-ido.el
@@ -0,0 +1,17 @@
+;; Use C-f during file selection to switch to regular find-file
+
+(require 'ido)
+(ido-mode t)
+(ido-everywhere t)
+(setq ido-enable-flex-matching t)
+(setq ido-use-filename-at-point nil)
+(setq ido-auto-merge-work-directories-length 0)
+(setq ido-use-virtual-buffers t)
+
+;; Allow the same buffer to be open in different frames
+(setq ido-default-buffer-method 'selected-window)
+
+;; http://www.reddit.com/r/emacs/comments/21a4p9/use_recentf_and_ido_together/cgbprem
+(add-hook 'ido-setup-hook (lambda () (define-key ido-completion-map [up] 'previous-history-element)))
+
+(provide 'init-ido)
