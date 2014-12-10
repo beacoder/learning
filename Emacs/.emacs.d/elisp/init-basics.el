ffd21dbf2c36e258146df2768f3a01cf78b6a76a
陈虎明 <chenhuming@gmail.com>
2014-12-10 15:53:40 +0800

    Update init-basics.el

--- Emacs/.emacs.d/elisp/init-basics.el
+++ Emacs/.emacs.d/elisp/init-basics.el
@@ -48,6 +48,32 @@
 (global-set-key (kbd "<C-S-right>")  'buf-move-right)
 
 ;;----------------------------------------------------------------------------
+;; Navigate window layouts with "C-c <left>" and "C-c <right>"
+;;----------------------------------------------------------------------------
+(winner-mode 1)
+
+;;----------------------------------------------------------------------------
+;; Make "C-x o" prompt for a target window when there are more than 2
+;;----------------------------------------------------------------------------
+(require-package 'switch-window)
+(require 'switch-window)
+(setq switch-window-shortcut-style 'alphabet)
+(global-set-key (kbd "C-x o") 'switch-window)
+
+;;----------------------------------------------------------------------------
+;; When splitting window, show (other-buffer) in the new window
+;;----------------------------------------------------------------------------
+(defun split-window-func-with-other-buffer (split-function)
+  (lexical-let ((s-f split-function))
+    (lambda ()
+      (interactive)
+      (funcall s-f)
+      (set-window-buffer (next-window) (other-buffer)))))
+
+(global-set-key "\C-x2" (split-window-func-with-other-buffer 'split-window-vertically))
+(global-set-key "\C-x3" (split-window-func-with-other-buffer 'split-window-horizontally))
+
+;;----------------------------------------------------------------------------
 ;; mode setting
 ;;----------------------------------------------------------------------------
 
