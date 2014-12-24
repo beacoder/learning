c0e86aeacec1cbeede69e058f27a06dad1b68207
陈虎明 <chenhuming@gmail.com>
2014-12-24 19:46:45 +0800

    Update init-nxml.el

--- Emacs/.emacs.d/elisp/init-nxml.el
+++ Emacs/.emacs.d/elisp/init-nxml.el
@@ -25,4 +25,25 @@ indentation rules."
  ;; prettify the xml in the active region
  (defalias 'pt 'sanityinc/pp-xml-region)
 
+;;----------------------------------------------------------------------------
+;; add fold/unfold support for xml files
+;;----------------------------------------------------------------------------
+
+(require 'hideshow)
+(require 'sgml-mode)
+(require 'nxml-mode)
+
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+
+;; optional key bindings, easier than hs defaults
+(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
+
 (provide 'init-nxml)
