9895ef623ed73a1e9bb12d5ba1b67d6adc6c58bb
陈虎明 <chenhuming@gmail.com>
2015-03-25 10:12:51 +0800

    Update init-modeline.el

--- Emacs/.emacs.d/elisp/init-modeline.el
+++ Emacs/.emacs.d/elisp/init-modeline.el
@@ -3,7 +3,10 @@
 ;;----------------------------------------------------------------------------
 
 ;; config time format
-(setq display-time-format "[%A %Y/%m/%d %H:%M Time-Zone:'%Z' %jth of %Y]")
+(setq display-time-format "[%A %Y/%m/%d %H:%M Time-Zone:'%Z' %jth of %Y]"
+      display-time-interval 60
+      display-time-default-load-average nil
+      display-time-mail-face 'custom-themed)
 
 ;; display time
 (display-time-mode t)
