231b05bbf783b8d51cac5c718d2982df1b9baa45
陈虎明 <chenhuming@gmail.com>
2015-04-28 09:59:14 +0800

    Update init-productivity.el

--- Emacs/.emacs.d/elisp/init-productivity.el
+++ Emacs/.emacs.d/elisp/init-productivity.el
@@ -204,4 +204,33 @@ See also: `xah-copy-to-register-1', `insert-register'."
 (global-set-key (kbd "C-c h 1") 'xah-copy-to-register-1)
 (global-set-key (kbd "C-c h 0") 'xah-paste-from-register-1)
 
+;;----------------------------------------------------------------------------
+;; search multiple open buffers
+;;----------------------------------------------------------------------------
+
+;; @see http://stackoverflow.com/questions/2641211/emacs-interactively-search-open-buffers
+(require 'cl)
+(defcustom search-all-buffers-ignored-files (list (rx-to-string '(and bos (or ".bash_history" "TAGS") eos)))
+  "Files to ignore when searching buffers via \\[search-all-buffers]."
+  :type 'editable-list)
+
+(require 'grep)
+(defun search-all-buffers (regexp prefix)
+  "Searches file-visiting buffers for occurence of REGEXP.  With
+prefix > 1 (i.e., if you type C-u \\[search-all-buffers]),
+searches all buffers."
+  (interactive (list (grep-read-regexp)
+                     current-prefix-arg))
+  (message "Regexp is %s; prefix is %s" regexp prefix)
+  (multi-occur
+   (if (member prefix '(4 (4)))
+       (buffer-list)
+     (remove-if
+      (lambda (b) (some (lambda (rx) (string-match rx  (file-name-nondirectory (buffer-file-name b)))) search-all-buffers-ignored-files))
+      (remove-if-not 'buffer-file-name (buffer-list))))
+
+   regexp))
+
+(global-set-key [f8] 'search-all-buffers)
+
 (provide 'init-productivity)
