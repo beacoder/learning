6d501841e65ae382681a0ef6bf764cc21c26c390
陈虎明 <chenhuming@gmail.com>
2015-09-18 09:29:47 +0800

    Update init-windows.el

--- Emacs/.emacs.d/elisp/init-windows.el
+++ Emacs/.emacs.d/elisp/init-windows.el
@@ -64,22 +64,4 @@
 (global-set-key "\C-x|" 'split-window-horizontally-instead)
 (global-set-key "\C-x_" 'split-window-vertically-instead)
 
-;; Borrowed from http://postmomentum.cn/blog/201304/blog-on-emacs
-(defun sanityinc/split-window()
-  "Split the window to see the most recent buffer in the other window.
-Call a second time to restore the original window configuration."
-  (interactive)
-  (if (eq last-command 'sanityinc/split-window)
-      (progn
-        (jump-to-register :sanityinc/split-window)
-        (setq this-command 'sanityinc/unsplit-window))
-    (window-configuration-to-register :sanityinc/split-window)
-    (switch-to-buffer-other-window nil)))
-
-(global-set-key (kbd "<f7>") 'sanityinc/split-window)
-(global-set-key (kbd "<f6>")
-                (lambda ()
-                  (interactive)
-                  (switch-to-buffer nil)))
-
 (provide 'init-windows)
