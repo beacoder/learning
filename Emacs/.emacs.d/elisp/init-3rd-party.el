2ff547c083aa28e2dd1e90ebbe74a7957adc7ac2
beacoder <chenhuming@gmail.com>
2016-03-07 09:50:43 +0800

    Update init-3rd-party.el

--- Emacs/.emacs.d/elisp/init-3rd-party.el
+++ Emacs/.emacs.d/elisp/init-3rd-party.el
@@ -92,6 +92,22 @@
 (require 'dired-details+)
 
 ;;----------------------------------------------------------------------------
+;; keyfreq setting
+;;----------------------------------------------------------------------------
+
+;; keyfreq-show could show the key-frequency
+(require 'keyfreq)
+(setq keyfreq-excluded-commands
+      '(self-insert-command
+        abort-recursive-edit
+        forward-char
+        backward-char
+        previous-line
+        next-line))
+(keyfreq-mode 1)
+(keyfreq-autosave-mode 1)
+
+;;----------------------------------------------------------------------------
 ;; third-party setting
 ;;----------------------------------------------------------------------------
 
