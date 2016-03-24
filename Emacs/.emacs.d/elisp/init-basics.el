a07f3cb9b2ee833198cf579eaee35fe485801fea
beacoder <chenhuming@gmail.com>
2016-03-24 15:08:20 +0800

    Update init-basics.el

--- Emacs/.emacs.d/elisp/init-basics.el
+++ Emacs/.emacs.d/elisp/init-basics.el
@@ -32,8 +32,7 @@ URL `https://sites.google.com/site/steveyegge2/effective-emacs'"
         (set-marker (mark-marker) pos)
         (setq mark-ring (nbutlast mark-ring))
         (goto-char (marker-position (car (last mark-ring))))))))
-(global-set-key (kbd "C-x p") 'pop-to-mark-command)
-(global-set-key (kbd "C-x n") 'unpop-to-mark-command)
+(global-set-key (kbd "C-c n") 'unpop-to-mark-command)
 (setq set-mark-command-repeat-pop t)
 
 ;; bind goto-line command
