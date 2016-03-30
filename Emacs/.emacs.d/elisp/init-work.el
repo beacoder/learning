935cd7071602d4c4e0722f863e5539d1dba2cdc8
beacoder <chenhuming@gmail.com>
2016-03-30 17:09:21 +0800

    Update init-work.el

--- Emacs/.emacs.d/elisp/init-work.el
+++ Emacs/.emacs.d/elisp/init-work.el
@@ -2,21 +2,28 @@
 ;; work related settings
 ;;----------------------------------------------------------------------------
 
-(defun update-ttcn3-tags ()
-  "Update ttcn3 tags"
+(require 'async)
+(defun async-update-ttcn3-tags ()
+  "Async udpate TTCN3 tags"
   (interactive)
-  (progn
-    ;; generate ttcn3 tags
-    (shell-command (concat (getenv "TTCN3_GGSN_ROOT_PATH") "/scripts/compile_ttcn.sh tags"))
-    (with-temp-file
-        "~/my_tag_files/TAGS"
-      (insert-file-contents (concat (getenv "TTCN3_BUILD_PATH") "/TAGS"))
-      (goto-char (point-min))
-      ;; replace relative path with abosolute path
-      (while (re-search-forward "^\.\./" nil t)
-        (replace-match (concat (getenv "TTCN3_GGSN_ROOT_PATH") "/") t nil))
-      (message "ttcn3 tags has been updated !"))))
-(defalias 'ut 'update-ttcn3-tags)
+  (let ()
+    (message "Updating TTCN3 tags...")
+    (async-start
+     ;; START-FUNC -> Building TTCN3 tags
+     `(lambda ()
+        ;; (require 'smtpmail)
+        (shell-command
+         (concat (getenv "TTCN3_GGSN_ROOT_PATH") "/scripts/compile_ttcn.sh tags")))
+     ;; FINISH-FUNC -> Change relative path to absolute path
+     `(lambda (&optional ignore)
+        (with-temp-file
+            "~/my_tag_files/TAGS"
+          (insert-file-contents (concat (getenv "TTCN3_BUILD_PATH") "/TAGS"))
+          (goto-char (point-min))
+          ;; replace relative path with abosolute path
+          (while (re-search-forward "^\.\./" nil t)
+            (replace-match (concat (getenv "TTCN3_GGSN_ROOT_PATH") "/") t nil))
+          (message "TTCN3 tags has been updated..."))))))
 
 (eval-after-load 'ttcn3
   '(progn
@@ -26,6 +33,6 @@
 ;; key-bindings used in ttcn3-mode
 (define-prefix-command 'ttcn3-map)
 (define-key ttcn3-mode-map "\C-xt" ttcn3-map)
-(define-key ttcn3-map (kbd "u") 'update-ttcn3-tags)
+(define-key ttcn3-map (kbd "u") 'async-update-ttcn3-tags)
 
 (provide 'init-work)
