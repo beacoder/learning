d7ea9e3f6046c6f2c0c33f7d669902c8ffc167c8
beacoder <chenhuming@gmail.com>
2016-03-30 15:04:18 +0800

    Update init-work.el

--- Emacs/.emacs.d/elisp/init-work.el
+++ Emacs/.emacs.d/elisp/init-work.el
@@ -8,13 +8,14 @@
   (progn
     ;; generate ttcn3 tags
     (shell-command (concat (getenv "TTCN3_GGSN_ROOT_PATH") "/scripts/compile_ttcn.sh tags"))
-    ;; replace relative path with abosolute path
     (with-temp-file
         "~/my_tag_files/TAGS"
       (insert-file-contents (concat (getenv "TTCN3_BUILD_PATH") "/TAGS"))
       (goto-char (point-min))
-      (replace-regexp "^\.\." (getenv "TTCN3_GGSN_ROOT_PATH")))
-    (message "ttcn3 tags has been updated !")))
+      ;; replace relative path with abosolute path
+      (while (re-search-forward "^\.\./" nil t)
+        (replace-match (concat (getenv "TTCN3_GGSN_ROOT_PATH") "/") t nil))
+      (message "ttcn3 tags has been updated !"))))
 (defalias 'ut 'update-ttcn3-tags)
 
 (provide 'init-work)
