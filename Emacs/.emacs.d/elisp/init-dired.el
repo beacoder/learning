32ade2d4bf33db384ce7f8612f8293e990b6ea93
beacoder <chenhuming@gmail.com>
2016-04-10 19:16:28 +0800

    Update init-dired.el

--- Emacs/.emacs.d/elisp/init-dired.el
+++ Emacs/.emacs.d/elisp/init-dired.el
@@ -8,14 +8,18 @@
 ;; allow dired to be able to delete or copy a whole dir.
 ;; "always" means no asking. "top" means ask once.
 ;; Any other symbol means ask each and every time for a dir and subdir.
-(setq dired-recursive-copies (quote always))
-(setq dired-recursive-deletes (quote top))
+(setq dired-recursive-copies (quote always)
+      dired-recursive-deletes (quote top))
 
 ;; copy from one dired dir to the next dired dir shown in a split window
 (setq dired-dwim-target t)
 
 ;; @see http://ergoemacs.org/emacs/dired_sort.html
 (setq dired-listing-switches "-alc --si --time-style long-iso")
+
+;; Auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t
+      auto-revert-verbose nil)
 
 ;; make dired use the same buffer for viewing directory, instead of spawning many
 ;; (add-hook 'dired-mode-hook
