4d91cd62619f49b96406bed7a320c52983fea6d4
beacoder <chenhuming@gmail.com>
2016-04-14 10:58:54 +0800

    Update init-utils.el

--- Emacs/.emacs.d/elisp/init-utils.el
+++ Emacs/.emacs.d/elisp/init-utils.el
@@ -145,4 +145,12 @@ White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
   (if (and (>= emacs-major-version 24) (>= emacs-minor-version 3))
       t nil))
 
+;;----------------------------------------------------------------------------
+;; Handier way to add modes to auto-mode-alist
+;;----------------------------------------------------------------------------
+(defun add-auto-mode (mode &rest patterns)
+  "Add entries to `auto-mode-alist' to use `MODE' for all given file `PATTERNS'."
+  (dolist (pattern patterns)
+    (add-to-list 'auto-mode-alist (cons pattern mode))))
+
 (provide 'init-utils)
