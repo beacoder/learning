a80886c580b84abf89f96e470181a9b658d75c98
beacoder <chenhuming@gmail.com>
2016-04-14 13:06:54 +0800

    Update init-utils.el

--- Emacs/.emacs.d/elisp/init-utils.el
+++ Emacs/.emacs.d/elisp/init-utils.el
@@ -152,7 +152,7 @@ White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
   "Add entries to `auto-mode-alist' to use `MODE' for all given file `PATTERNS'."
   (dolist (pattern patterns)
     (add-to-list 'auto-mode-alist (cons pattern mode))))
-    
+
 (if (fboundp 'with-eval-after-load)
     (defalias 'after-load 'with-eval-after-load)
   (defmacro after-load (feature &rest body)
