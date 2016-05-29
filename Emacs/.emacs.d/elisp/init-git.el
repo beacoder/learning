020a4cf5b9db985f74d4418e27ac6e4cc6afabc4
beacoder <chenhuming@gmail.com>
2016-05-29 16:44:13 +0800

    add magit config file

---
+++ Emacs/.emacs.d/elisp/init-git.el
@@ -0,0 +1,40 @@
+;;----------------------------------------------------------------------------
+;; Magit setting
+;;----------------------------------------------------------------------------
+
+;; TODO: link commits from vc-log to magit-show-commit
+;; TODO: smerge-mode
+(require-package 'git-blame)
+(require-package 'gitignore-mode)
+(require-package 'gitconfig-mode)
+(require-package 'git-messenger) ;; Though see also vc-annotate's "n" & "p" bindings
+(require-package 'git-timemachine)
+
+
+(when (maybe-require-package 'magit)
+  (setq-default
+   magit-process-popup-time 10
+   magit-diff-refine-hunk t
+   magit-completing-read-function 'magit-ido-completing-read)
+
+  ;; quickly open magit on any one of your projects.
+  (global-set-key (kbd "C-x g") 'magit-status)
+  (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup))
+
+(after-load 'magit
+  (define-key magit-status-mode-map (kbd "C-M-<up>") 'magit-section-up)
+  (add-hook 'magit-popup-mode-hook (lambda () (setq show-trailing-whitespace nil))))
+
+(require-package 'fullframe)
+(after-load 'magit
+  (fullframe magit-status magit-mode-quit-window))
+
+(when (maybe-require-package 'git-commit)
+  (add-hook 'git-commit-mode-hook 'goto-address-mode))
+
+
+(require-package 'git-messenger)
+(global-set-key (kbd "C-x v p") #'git-messenger:popup-message)
+
+
+(provide 'init-git)
