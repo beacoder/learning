9dcf56e260c1c5974cf28a961e491141c42c07c4
陈虎明 <chenhuming@gmail.com>
2017-01-04 10:37:00 +0800

    Update init-ido.el

--- Emacs/.emacs.d/elisp/init-ido.el
+++ Emacs/.emacs.d/elisp/init-ido.el
@@ -2,15 +2,21 @@
 ;; ido configuration (use helm if possible)
 ;;----------------------------------------------------------------------------
 
+(require-package 'flx-ido)
+(flx-ido-mode 1)
+;; @see https://github.com/lewang/flx
+(setq flx-ido-threshold 10000)
+
 ;; use "C-f" during file selection to switch to regular find-file
 (ido-mode t)
-(ido-everywhere t)
+
 (setq ido-enable-flex-matching t
       ido-use-filename-at-point nil
       ido-auto-merge-work-directories-length -1
-      ido-create-new-buffer 'always
+      ido-use-faces nil          ;; disable ido faces to see flx highlights.
+      ;; ido-create-new-buffer 'always
       ido-use-virtual-buffers t
-      ;; ido-separator "\n" ;; display choices vertically
+      ;; ido-separator "\n"      ;; display choices vertically
       ido-default-buffer-method 'selected-window)
 
 (when (maybe-require-package 'ido-ubiquitous)
@@ -29,50 +35,33 @@
 ;; Ignore .DS_Store files with ido mode
 (add-to-list 'ido-ignore-files "\\.DS_Store")
 
-;; steal from https://github.com/redguardtoo/emacs.d/blob/master/lisp/init-ido.el
-(defun ido-imenu ()
-  "Update the imenu index and then use ido to select a symbol to navigate to.
-Symbols matching the text at point are put first in the completion list."
-  (interactive)
-  (unless (featurep 'imenu)
-    (require 'imenu nil t))
-  (imenu--make-index-alist)
-  (let ((name-and-pos '())
-        (symbol-names '()))
-    (flet ((addsymbols (symbol-list)
-                       (when (listp symbol-list)
-                         (dolist (symbol symbol-list)
-                           (let ((name nil) (position nil))
-                             (cond
-                              ((and (listp symbol) (imenu--subalist-p symbol))
-                               (addsymbols symbol))
-
-                              ((listp symbol)
-                               (setq name (car symbol))
-                               (setq position (cdr symbol)))
-
-                              ((stringp symbol)
-                               (setq name symbol)
-                               (setq position (get-text-property 1 'org-imenu-marker symbol))))
-
-                             (unless (or (null position) (null name))
-                               (add-to-list 'symbol-names name)
-                               (add-to-list 'name-and-pos (cons name position))))))))
-      (addsymbols imenu--index-alist))
-    ;; If there are matching symbols at point, put them at the beginning of `symbol-names'.
-    (let ((symbol-at-point (thing-at-point 'symbol)))
-      (when symbol-at-point
-        (let* ((regexp (concat (regexp-quote symbol-at-point) "$"))
-               (matching-symbols (delq nil (mapcar (lambda (symbol)
-                                                     (if (string-match regexp symbol) symbol))
-                                                   symbol-names))))
-          (when matching-symbols
-            (sort matching-symbols (lambda (a b) (> (length a) (length b))))
-            (mapc (lambda (symbol) (setq symbol-names (cons symbol (delete symbol symbol-names))))
-                  matching-symbols)))))
-    (let* ((selected-symbol (ido-completing-read "Symbol? " symbol-names))
-           (position (cdr (assoc selected-symbol name-and-pos))))
-      (push-mark (point))
-      (goto-char position))))
+;; disable ido for certain commands,
+;; @see http://stackoverflow.com/questions/6771664/disable-ido-mode-for-specific-commands
+(defadvice ido-read-buffer (around ido-read-buffer-possibly-ignore activate)
+  "Check to see if use wanted to avoid using ido"
+  (if (eq (get this-command 'ido) 'ignore)
+      (let ((read-buffer-function nil))
+        (run-hook-with-args 'ido-before-fallback-functions 'read-buffer)
+        (setq ad-return-value (apply 'read-buffer (ad-get-args 0))))
+    ad-do-it))
+(put 'shell 'ido 'ignore)
+(put 'ffap-alternate-file 'ido 'ignore)
+(put 'tmm-menubar 'ido 'ignore)
+(put 'dired-do-copy 'ido 'ignore)
+(put 'dired-do-rename 'ido 'ignore)
+(put 'vc-copy-file-and-rename-buffer 'ido 'ignore)
+(put 'dired-create-directory 'ido 'ignore)
+(put 'copy-file-and-rename-buffer 'ido 'ignore)
+(put 'rename-file-and-buffer 'ido 'ignore)
+(put 'w3m-goto-url 'ido 'ignore)
+(put 'ido-find-file 'ido 'ignore)
+(put 'ido-edit-input 'ido 'ignore)
+(put 'read-file-name 'ido 'ignore)
+(put 'dired-create-directory 'ido 'ignore)
+(put 'minibuffer-completion-help 'ido 'ignore)
+(put 'minibuffer-complete 'ido 'ignore)
+(put 'c-set-offset 'ido 'ignore)
+(put 'rgrep 'ido 'ignore)
+(put 'dired-create-directory 'ido 'ignore)
 
 (provide 'init-ido)
