3ed8841af5665e8af159978b63dc1049f68cb295
陈虎明 <chenhuming@gmail.com>
2017-04-22 02:45:20 +0800

    Update init-hydra.el

--- Emacs/.emacs.d/elisp/init-hydra.el
+++ Emacs/.emacs.d/elisp/init-hydra.el
@@ -7,11 +7,26 @@
 ;; Hydra groups related commands together to act like a temporary minor mode
 (require-package 'hydra)
 
-;; Customize keybindings for multiple-cursors-mode.
-(defhydra hydra/mc-mode ()
-   ("p" mc/mark-previous-like-this)
-   ("n" mc/mark-next-like-this))
-(global-set-key (kbd "C-x m")  'hydra/mc-mode/body)
+(defhydra multiple-cursors-hydra (:hint nil)
+  "
+     ^Up^            ^Down^        ^Other^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
+^ ^             ^ ^             [_q_] Quit
+"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("r" mc/mark-all-in-region-regexp :exit t)
+  ("q" nil))
+(global-set-key (kbd "C-x m")  'multiple-cursors-hydra/body)
 
 (defhydra hydra/window-movement ()
   ("<left>" windmove-left)
