46bb15ed9c447516142f22831720f90fcced407a
陈虎明 <chenhuming@gmail.com>
2017-04-22 09:40:07 +0800

    Update init-hydra.el

--- Emacs/.emacs.d/elisp/init-hydra.el
+++ Emacs/.emacs.d/elisp/init-hydra.el
@@ -30,63 +30,37 @@
 
 (defhydra hydra-window ()
    "
-Movement^^        ^Split^         ^Switch^		^Resize^
-----------------------------------------------------------------
-_h_ ←           _v_ertical      _b_uffer		_q_ X←
-_j_ ↓           _x_ horizontal	_f_ind files	_w_ X↓
-_k_ ↑           _z_ undo        _a_ce 1		_e_ X↑
-_l_ →           _Z_ reset       _s_wap		_r_ X→
-_F_ollow		_D_lt Other     _S_ave		max_i_mize
-_SPC_ cancel	_o_nly this     _d_elete
+    ^Move^        ^Split^             ^Switch^          ^Other^
+-------------------------------------------------------------
+[_h_] ←       [_v_] vertical      [_b_] buffer      [_q_] quit
+[_j_] ↓       [_x_] horizontal    [_f_] find files
+[_k_] ↑       [_z_] undo          [_S_] save
+[_l_] →       [_Z_] reset         [_d_] delete
+[_F_] follow  [_o_] only this
 "
-   ("h" windmove-left )
-   ("j" windmove-down )
-   ("k" windmove-up )
-   ("l" windmove-right )
-   ("q" hydra-move-splitter-left)
-   ("w" hydra-move-splitter-down)
-   ("e" hydra-move-splitter-up)
-   ("r" hydra-move-splitter-right)
+   ("h" windmove-left)
+   ("j" windmove-down)
+   ("k" windmove-up)
+   ("l" windmove-right)
    ("b" helm-mini)
    ("f" helm-find-files)
    ("F" follow-mode)
-   ("a" (lambda ()
-          (interactive)
-          (ace-window 1)
-          (add-hook 'ace-window-end-once-hook
-                    'hydra-window/body))
-       )
    ("v" (lambda ()
           (interactive)
           (split-window-right)
-          (windmove-right))
-       )
+          (windmove-right)))
    ("x" (lambda ()
           (interactive)
           (split-window-below)
-          (windmove-down))
-       )
-   ("s" (lambda ()
-          (interactive)
-          (ace-window 4)
-          (add-hook 'ace-window-end-once-hook
-                    'hydra-window/body)))
+          (windmove-down)))
    ("S" save-buffer)
    ("d" delete-window)
-   ("D" (lambda ()
-          (interactive)
-          (ace-window 16)
-          (add-hook 'ace-window-end-once-hook
-                    'hydra-window/body))
-       )
    ("o" delete-other-windows)
-   ("i" ace-maximize-window)
    ("z" (progn
           (winner-undo)
-          (setq this-command 'winner-undo))
-   )
+          (setq this-command 'winner-undo)))
    ("Z" winner-redo)
-   ("SPC" nil))
+   ("q" nil))
 (global-set-key (kbd "C-x w") 'hydra-window/body)
 
 (defhydra hydra/rectangle (:pre (rectangle-mark-mode 1)
