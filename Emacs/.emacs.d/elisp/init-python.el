62075daa3392d625ec8769dfcbab5950d1b888a7
陈虎明 <chenhuming@gmail.com>
2017-05-08 23:38:38 +0800

    Update init-python.el

--- Emacs/.emacs.d/elisp/init-python.el
+++ Emacs/.emacs.d/elisp/init-python.el
@@ -2,6 +2,23 @@
 ;; python programming mode setting
 ;;----------------------------------------------------------------------------
 
+;; "C-c C-p"    =>  run-python
+;; "C-c C-c"    =>  python-shell-send-buffer
+;; "C-c C-j"    =>  imenu
+;; "C-c C-r"    =>  python-shell-send-region
+;; "C-c C-z"    =>  python-shell-switch-to-shell
+
+;; "C-c <"      =>  python-indent-shift-left
+;; "C-c >"      =>  python-indent-shift-right
+
+;; "C-c C-t c"  =>  python-skeleton-class
+;; "C-c C-t d"  =>  python-skeleton-def
+;; "C-c C-t f"  =>  python-skeleton-for
+;; "C-c C-t i"  =>  python-skeleton-if
+;; "C-c C-t m"  =>  python-skeleton-import
+;; "C-c C-t t"  =>  python-skeleton-try
+;; "C-c C-t w"  =>  python-skeleton-while
+
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
 		("SConscript\\'" . python-mode))
