9a10cffdcf3ed154180a4ee673cd2cd2729fe1fa
陈虎明 <chenhuming@gmail.com>
2017-07-14 16:01:40 +0800

    Update init-hydra.el

--- Emacs/.emacs.d/elisp/init-hydra.el
+++ Emacs/.emacs.d/elisp/init-hydra.el
@@ -68,14 +68,14 @@
   "
                  ^Commands^
 -----------------------------------------
-[_s_] Swiper-at-point [_S_] Counsel-ag-project
+[_s_] Swiper-at-point [_a_] Counsel-ag-project
 [_l_] Counsel-locate  [_f_] Flymake-mode
 [_u_] Update-GTAGS    [_c_] Mode-Compile
 [_C_] Compile         [_r_] Recompile
 [_q_] Quit
 "
   ("s" sanityinc/swiper-at-point :exit t)
-  ("S" sanityinc/counsel-ag-project :exit t)
+  ("a" sanityinc/counsel-ag-project :exit t)
   ("l" counsel-locate :exit t)
   ("f" flymake-mode :exit t)
   ("u" ggtags-update-tags :exit t)
