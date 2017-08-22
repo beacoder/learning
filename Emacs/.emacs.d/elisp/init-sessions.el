8ba387872258fc66cfd981544dda60e9c76cb6b6
陈虎明 <chenhuming@gmail.com>
2017-08-22 10:27:45 +0800

    Update init-sessions.el

--- Emacs/.emacs.d/elisp/init-sessions.el
+++ Emacs/.emacs.d/elisp/init-sessions.el
@@ -4,14 +4,29 @@
 ;; save a list of open files in ~/.emacs.d/.emacs.desktop
 ;; save the desktop file automatically if it already exists
 ;; we need to manually execute 'desktop-save' for the first time
-
-(setq desktop-path '("~/.emacs.d"))
-(setq desktop-save 'if-exists)
-(if (fboundp 'desktop-save-mode)
+(setq desktop-path (list user-emacs-directory)
+      desktop-auto-save-timeout 600)
+(when (fboundp 'desktop-save-mode)
     (desktop-save-mode 1))
-(defadvice desktop-read (around trace-desktop-errors)
-  (let ((debug-on-error t))
-    ad-do-it))
+
+(defadvice desktop-read (around time-restore activate)
+  (let ((start-time (current-time)))
+    (prog1
+        ad-do-it
+      (message "Desktop restored in %.2fms"
+               (sanityinc/time-subtract-millis (current-time)
+                                               start-time)))))
+
+(defadvice desktop-create-buffer (around time-create activate)
+  (let ((start-time (current-time))
+        (filename (ad-get-arg 1)))
+    (prog1
+        ad-do-it
+      (message "Desktop: %.2fms to restore %s"
+               (sanityinc/time-subtract-millis (current-time)
+                                               start-time)
+               (when filename
+                 (abbreviate-file-name filename))))))
 
 ;;----------------------------------------------------------------------------
 ;; Restore histories and registers after saving
