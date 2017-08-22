5d8806b1c5dd3570683886ea604a49e602da1d82
陈虎明 <chenhuming@gmail.com>
2017-08-22 10:18:27 +0800

    Update init-sessions.el

--- Emacs/.emacs.d/elisp/init-sessions.el
+++ Emacs/.emacs.d/elisp/init-sessions.el
@@ -16,8 +16,13 @@
 ;;----------------------------------------------------------------------------
 ;; Restore histories and registers after saving
 ;;----------------------------------------------------------------------------
+(setq history-length 1000)
+(savehist-mode t)
+
 (require 'session)
+
 ;; (setq session-save-file (expand-file-name "~/.emacs.d/.session"))
+(setq session-name-disable-regexp "\\(?:\\`'/tmp\\|\\.git/[A-Z_]+\\'\\)")
 (add-hook 'after-init-hook 'session-initialize)
 
 ;; buffers don't need to be saved.
@@ -27,25 +32,33 @@
 ;; save a bunch of variables to the desktop file
 ;; for lists specify the len of the maximal saved data also
 (setq desktop-globals-to-save
-      (append '((extended-command-history . 30)
+      (append '((comint-input-ring        . 50)
+                (compile-history          . 30)
+                desktop-missing-file-warning
+                (dired-regexp-history     . 20)
+                (extended-command-history . 30)
+                (face-name-history        . 20)
                 (file-name-history        . 100)
+                (grep-find-history        . 30)
+                (grep-history             . 30)
+                (ido-buffer-history       . 100)
                 (ido-last-directory-list  . 100)
                 (ido-work-directory-list  . 100)
                 (ido-work-file-list       . 100)
+                (ivy-history              . 100)
                 (magit-read-rev-history   . 50)
-                (grep-history             . 30)
-                (compile-history          . 30)
                 (minibuffer-history       . 50)
+                (org-clock-history        . 50)
+                (org-refile-history       . 50)
+                (org-tags-history         . 50)
                 (query-replace-history    . 60)
                 (read-expression-history  . 60)
                 (regexp-history           . 60)
                 (regexp-search-ring       . 20)
+                register-alist
                 (search-ring              . 20)
-                (comint-input-ring        . 50)
                 (shell-command-history    . 50)
-                (evil-ex                  .100)
-                desktop-missing-file-warning
                 tags-file-name
-                register-alist)))
+                tags-table-list)))
 
 (provide 'init-sessions)
