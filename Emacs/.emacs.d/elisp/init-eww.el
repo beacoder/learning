4692f85e30cb7c34a575855a07a5ee670c111dd0
陈虎明 <chenhuming@gmail.com>
2017-10-17 11:13:37 +0800

    Update init-eww.el

--- Emacs/.emacs.d/elisp/init-eww.el
+++ Emacs/.emacs.d/elisp/init-eww.el
@@ -4,46 +4,6 @@
 
 ;; @see https://github.com/kaushalmodi/.emacs.d/blob/master/setup-files/setup-eww.el
 
-;; Default eww key bindings
-;; |-----------+---------------------------------------------------------------------|
-;; | Key       | Function                                                            |
-;; |-----------+---------------------------------------------------------------------|
-;; | &         | Browse the current URL with an external browser.                    |
-;; | -         | Begin a negative numeric argument for the next command.             |
-;; | 0 .. 9    | Part of the numeric argument for the next command.                  |
-;; | C         | Display a buffer listing the current URL cookies, if there are any. |
-;; | H         | List the eww-histories.                                             |
-;; | F         | Toggle font between variable-width and fixed-width.                 |
-;; | G         | Go to a URL                                                         |
-;; | R         | Readable mode                                                       |
-;; | S         | List eww buffers                                                    |
-;; | d         | Download URL under point to `eww-download-directory'.               |
-;; | g         | Reload the current page.                                            |
-;; | q         | Quit WINDOW and bury its buffer.                                    |
-;; | v         | `eww-view-source'                                                   |
-;; | w         | `eww-copy-page-url'                                                 |
-;; |-----------+---------------------------------------------------------------------|
-;; | b         | Add the current page to the bookmarks.                              |
-;; | B         | Display the bookmark list.                                          |
-;; | M-n       | Visit the next bookmark                                             |
-;; | M-p       | Visit the previous bookmark                                         |
-;; |-----------+---------------------------------------------------------------------|
-;; | t         | Go to the page marked `top'.                                        |
-;; | u         | Go to the page marked `up'.                                         |
-;; |-----------+---------------------------------------------------------------------|
-;; | n         | Go to the page marked `next'.                                       |
-;; | p         | Go to the page marked `previous'.                                   |
-;; |-----------+---------------------------------------------------------------------|
-;; | l         | Go to the previously displayed page.                                |
-;; | r         | Go to the next displayed page.                                      |
-;; |-----------+---------------------------------------------------------------------|
-;; | TAB       | Move point to next link on the page.                                |
-;; | S-TAB     | Move point to previous link on the page.                            |
-;; |-----------+---------------------------------------------------------------------|
-;; | SPC       | Scroll up                                                           |
-;; | DEL/Bkspc | Scroll down                                                         |
-;; | S-SPC     | Scroll down                                                         |
-;; |-----------+---------------------------------------------------------------------|
 
 ;; eww-lnum
 ;; https://github.com/m00natic/eww-lnum
