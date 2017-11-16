9c1862d3e552416012c56d095b61429106c8aea8
陈虎明 <chenhuming@gmail.com>
2017-11-16 17:02:10 +0800

    Update init-dired.el

--- Emacs/.emacs.d/elisp/init-dired.el
+++ Emacs/.emacs.d/elisp/init-dired.el
@@ -38,9 +38,9 @@
 (let ((gls (executable-find "gls")))
   (when gls (setq insert-directory-program gls)))
 
-(when (maybe-require-package 'diredfl)
-  (after-load 'dired
-    (diredfl-global-mode)))
+;; (when (maybe-require-package 'diredfl)
+;;   (after-load 'dired
+;;     (diredfl-global-mode)))
 
 (after-load 'dired
   (require 'dired+)
