68fdf78933d1183ea1d1ef9bebcdbb567b93e2e5
陈虎明 <chenhuming@gmail.com>
2017-11-30 10:24:53 +0800

    Update init-python.el

--- Emacs/.emacs.d/elisp/init-python.el
+++ Emacs/.emacs.d/elisp/init-python.el
@@ -24,19 +24,23 @@
     (add-hook 'python-mode-hook 'anaconda-mode)
     (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
 
-    (define-key python-mode-map (kbd "C-c c") 'python-skeleton-class)
-    (define-key python-mode-map (kbd "C-c d") 'python-skeleton-def)
-    (define-key python-mode-map (kbd "C-c f") 'python-skeleton-for)
-    (define-key python-mode-map (kbd "C-c i") 'python-skeleton-if)
-    (define-key python-mode-map (kbd "C-c m") 'python-skeleton-import)
-    (define-key python-mode-map (kbd "C-c t") 'python-skeleton-try)
-    (define-key python-mode-map (kbd "C-c w") 'python-skeleton-while))
+    (bind-keys
+     :map python-mode-map
+     ("C-c c" . python-skeleton-class)
+     ("C-c d" . python-skeleton-def)
+     ("C-c f" . python-skeleton-for)
+     ("C-c i" . python-skeleton-if)
+     ("C-c m" . python-skeleton-import)
+     ("C-c t" . python-skeleton-try)
+     ("C-c w" . python-skeleton-while)))
 
   (after-load 'anaconda-mode
-    (define-key anaconda-mode-map (kbd "M-?") 'anaconda-mode-go-back)
-    (define-key anaconda-mode-map (kbd "M-]") 'anaconda-mode-find-references)
-    (define-key anaconda-mode-map (kbd "M-=") 'anaconda-mode-find-assignments)
-    (define-key anaconda-mode-map (kbd "M-,") 'anaconda-mode-show-doc))
+    (bind-keys
+     :map anaconda-mode-map
+     ("M-?" . anaconda-mode-go-back)
+     ("M-]" . anaconda-mode-find-references)
+     ("M-=" . anaconda-mode-find-assignments)
+     ("M-," . anaconda-mode-show-doc)))
 
   (when (maybe-require-package 'company-anaconda)
     (after-load 'company
