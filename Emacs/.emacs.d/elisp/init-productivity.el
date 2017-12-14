11f33687cd5adfdd8915c7f3f11980c2d905080e
陈虎明 <chenhuming@gmail.com>
2017-12-14 10:47:55 +0800

    Update init-productivity.el

--- Emacs/.emacs.d/elisp/init-productivity.el
+++ Emacs/.emacs.d/elisp/init-productivity.el
@@ -26,45 +26,6 @@ This command is similar to `find-file-at-point' but without prompting for confir
               (find-file path ))))))))
 
 ;;----------------------------------------------------------------------------
-;; extend-selection
-;;----------------------------------------------------------------------------
-
-;; by Nikolaj Schumacher, 2008-10-20. Released under GPL.
-(global-set-key (kbd "M-8") 'extend-selection)
-(defun semnav-up (arg)
-  (interactive "p")
-  (when (nth 3 (syntax-ppss))
-    (if (> arg 0)
-        (progn
-          (skip-syntax-forward "^\"")
-          (goto-char (1+ (point)))
-          (decf arg))
-      (skip-syntax-backward "^\"")
-      (goto-char (1- (point)))
-      (incf arg)))
-  (up-list arg))
-
-;; by Nikolaj Schumacher, 2008-10-20. Released under GPL.
-(defun extend-selection (arg &optional incremental)
-  "Select the current word.
-Subsequent calls expands the selection to larger semantic unit."
-  (interactive (list (prefix-numeric-value current-prefix-arg)
-                     (or (use-region-p)
-                         (eq last-command this-command))))
-  (if incremental
-      (progn
-        (semnav-up (- arg))
-        (forward-sexp)
-        (mark-sexp -1))
-    (if (> arg 1)
-        (extend-selection (1- arg) t)
-      (if (looking-at "\\=\\(\\s_\\|\\sw\\)*\\_>")
-          (goto-char (match-end 0))
-        (unless (memq (char-before) '(?\) ?\"))
-          (forward-sexp)))
-      (mark-sexp -1))))
-
-;;----------------------------------------------------------------------------
 ;; register-usage
 ;;----------------------------------------------------------------------------
 
