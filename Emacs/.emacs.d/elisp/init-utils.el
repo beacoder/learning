8948064a646ed5de0f1f884eb23e50504a3348f9
陈虎明 <chenhuming@gmail.com>
2017-12-27 11:02:42 +0800

    Update init-utils.el

--- Emacs/.emacs.d/elisp/init-utils.el
+++ Emacs/.emacs.d/elisp/init-utils.el
@@ -206,4 +206,35 @@ If there's a string at point, use it instead of prompt."
     (and buf (not (buffer-modified-p buf))
          (kill-buffer buf))))
 
+
+;;; simplicistic queue implementation
+;;; @see http://ix.io/DoE
+(defun list-to-queue (list)
+  (cons list (if (consp list) (last list) '())))
+
+(defun queue-to-list (queue)
+  (car queue))
+
+(defun make-queue ()
+  (cons '() '()))
+
+(defun queue-empty-p (queue)
+  (null (car queue)))
+
+(defun queue-get (queue)
+  (if (null (car queue))
+      (error "Queue is empty")
+      (let ((x (caar queue)))
+        (setcar queue (cdar queue))
+        (if (null (car queue)) (setcdr queue '()))
+        x)))
+
+(defun queue-put (queue x)
+  (let ((entry (cons x '())))
+    (if (null (car queue))
+        (setcar queue entry)
+        (setcdr (cdr queue) entry))
+    (setcdr queue entry)
+    x))
+
 (provide 'init-utils)
