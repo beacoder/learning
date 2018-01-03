db20615c5b08c1625020481184a249a2431b02fc
陈虎明 <chenhuming@gmail.com>
2018-01-03 15:46:37 +0800

    Update init-nxml.el

--- Emacs/.emacs.d/elisp/init-nxml.el
+++ Emacs/.emacs.d/elisp/init-nxml.el
@@ -2,55 +2,49 @@
 ;; nxml mode configuration
 ;;----------------------------------------------------------------------------
 
+(add-auto-mode
+ 'nxml-mode
+ (concat "\\."
+         (regexp-opt
+          '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss"
+            "gpx" "tcx" "plist"))
+         "\\'"))
+
 (setq magic-mode-alist (cons '("<\\?xml " . nxml-mode) magic-mode-alist))
 (fset 'xml-mode 'nxml-mode)
-(add-hook 'nxml-mode-hook (lambda ()
+(add-hook 'nxml-mode-hook (λ ()
                             (set (make-local-variable 'ido-use-filename-at-point) nil)))
 (setq nxml-slash-auto-complete-flag t)
 
-;; @see: http://sinewalker.wordpress.com/2008/06/26/pretty-printing-xml-with-emacs-nxml-mode/
-(defun sanityinc/pp-xml-region (begin end)
+
+;; See: http://sinewalker.wordpress.com/2008/06/26/pretty-printing-xml-with-emacs-nxml-mode/
+(defun sanityinc/pp-xml-region (beg end)
   "Pretty format XML markup in region. The function inserts
 linebreaks to separate tags that have nothing but whitespace
 between them.  It then indents the markup by using nxml's
 indentation rules."
   (interactive "r")
+  (unless (use-region-p)
+    (setq beg (point-min)
+          end (point-max)))
+  ;; Use markers because our changes will move END
+  (setq beg (set-marker (make-marker) begin)
+        end (set-marker (make-marker) end))
   (save-excursion
-      (nxml-mode)
-      (goto-char begin)
-      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
-        (backward-char) (insert "\n"))
-      (indent-region begin end)))
+    (goto-char beg)
+    (while (search-forward-regexp "\>[ \\t]*\<" end t)
+      (backward-char) (insert "\n"))
+    (nxml-mode)
+    (indent-region begin end)))
 
 ;; prettify the xml in the active region
 (defalias 'pt 'sanityinc/pp-xml-region)
 
 ;;----------------------------------------------------------------------------
-;; add fold/unfold support for xml files
-;;----------------------------------------------------------------------------
-
-(require 'hideshow)
-(require 'sgml-mode)
-(require 'nxml-mode)
-
-(add-to-list 'hs-special-modes-alist
-             '(nxml-mode
-               "<!--\\|<[^/>]*[^/]>"
-               "-->\\|</[^/>]*[^/]>"
-
-               "<!--"
-               sgml-skip-tag-forward
-               nil))
-(add-hook 'nxml-mode-hook 'hs-minor-mode)
-
-;; optional key bindings, easier than hs defaults
-(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
-
-;;----------------------------------------------------------------------------
 ;; Integration with tidy for html + xml
 ;;----------------------------------------------------------------------------
 (require-package 'tidy)
-(add-hook 'nxml-mode-hook (lambda () (tidy-build-menu nxml-mode-map)))
+(add-hook 'nxml-mode-hook (λ () (tidy-build-menu nxml-mode-map)))
 
 (defun sanityinc/tidy-buffer-xml (beg end)
   "Run \"tidy -xml\" on the region from BEG to END, or whole buffer."
