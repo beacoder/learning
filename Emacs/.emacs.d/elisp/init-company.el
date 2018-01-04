abb44045af840c01bab248228d4c450288c471a5
陈虎明 <chenhuming@gmail.com>
2018-01-04 16:30:07 +0800

    Update init-company.el

--- Emacs/.emacs.d/elisp/init-company.el
+++ Emacs/.emacs.d/elisp/init-company.el
@@ -2,13 +2,10 @@
 ;; company-mode setting
 ;;----------------------------------------------------------------------------
 
-;; WAITING: haskell-mode sets tags-table-list globally, breaks tags-completion-at-point-function
-;; TODO Default sort order should place [a-z] before punctuation
-
 ;; (setq tab-always-indent 'complete)  ;; use 't when company is disabled
 (add-to-list 'completion-styles 'initials t)
-;; Stop completion-at-point from popping up completion buffers so eagerly
-(setq completion-cycle-threshold 5)
+(setq completion-cycle-threshold 5
+      company-show-numbers t)
 
 
 (when (maybe-require-package 'company)
