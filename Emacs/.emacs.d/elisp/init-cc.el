7db1d8c9c1c2a18a9190a30b610730f1459c4106
陈虎明 <chenhuming@gmail.com>
2018-01-16 17:22:59 +0800

    Add toggle flymake function.

--- Emacs/.emacs.d/elisp/init-cc.el
+++ Emacs/.emacs.d/elisp/init-cc.el
@@ -114,96 +114,21 @@ Key bindings:
     nil
     :keymap my-flymake-minor-mode-map)
 
-  ;; Enable this keybinding (my-flymake-minor-mode) by default
-  ;; Added by Hartmut 2011-07-05
-  (add-hook 'c-mode-common-hook 'my-flymake-minor-mode)
-  (add-hook 'c-mode-common-hook 'flymake-mode-on)
-  (add-hook 'c-mode-common-hook 'my-flymake-google-init))
-
-;;----------------------------------------------------------------------------
-;; generate the template c++ header and source files
-;;----------------------------------------------------------------------------
-
-(defun c++-create-class (name)
-  "Insert a C++ class definition.
- It creates a matching header file, inserts the class definition and
- creates the  most important function templates in a named after the
- class name. This might still be somewhat buggy."
-  (interactive "sclass name: ")
-  (let* ((header-file-name (concat name ".hpp"))
-		 (header-include-string (upcase (concat name "_H_INCLUDED")))
-		 (def-file-name    (concat name ".cpp")))
-
-	;; write header file
-	(set-buffer (get-buffer-create header-file-name))
-	(set-visited-file-name header-file-name)
-	(c++-mode)
-	(turn-on-font-lock)
-	(insert (concat
-			"/* -*-C++-*- */\n"
-                         "#ifndef " header-include-string "\n"
-                         "#define " header-include-string "\n\n"
-                         "////////////////////////////////////////////////////////////////////////////////\n"
-                         "//\n"
-                         "// File:         " header-file-name "\n"
-                         "// Description:   \n"
-                         "// Created:      " (substring (current-time-string) -4) "\n"
-                         "// Author: " (user-full-name) "\n"
-                         "//\n"
-                         "// (c) Copyright 2008, Advantest, all rights reserved.\n"
-                         "//\n"
-                         "////////////////////////////////////////////////////////////////////////////////\n\n"
-                         "#include <iostream>\n\n"
-                         "class " name "_I;\n\n"
-                         "class " name "\n"
-                         "{\n"
-                         "public:\n"
-                         name "();\n"
-                         "virtual ~" name "();\n"
-                         name "(const " name "& src);\n"
-                         name "& operator=(const " name "& rv);\n\n"
-                         "protected:\n"
-                         "void init();\n"
-                         "void reset();\n\n"
-                         "private: \n"
-                         name "_I * mImp;\n"
-                         "};\n\n"
-                         "# endif\n"))
-	(beginning-of-buffer)
-	(while (and (not (eobp)) (forward-line))
-	  (indent-according-to-mode))
-
-	;; create CC file
-	(set-buffer (get-buffer-create def-file-name))
-	(set-visited-file-name def-file-name)
-	(switch-to-buffer (current-buffer))
-	(c++-mode)
-	(turn-on-font-lock)
-	(insert (concat
-			 "/* -*-C++-*- */\n"
-                         "////////////////////////////////////////////////////////////////////////////////\n"
-                         "//\n"
-                         "// File:         " def-file-name "\n"
-                         "// Description:   \n"
-                         "// Created:      " (substring (current-time-string) -4) "\n"
-                         "// Author: " (user-full-name) "\n"
-                         "//\n"
-                         "// (c) Copyright 2008, Advantest, all rights reserved.\n"
-                         "//\n"
-                         "////////////////////////////////////////////////////////////////////////////////\n\n"
-                         "#include \"" header-file-name "\"\n"
-                         "#include \"" name "_I.hpp\"\n\n"
-                         name "::" name "()\n{\nmImp = new " name "_I();\n}\n\n"
-                         name "::~" name "()\n{\nif (NULL != mImp)\n{\ndelete mImp;\nmImp = NULL;\n}\n}\n\n"
-                         name "::" name "(const " name "& src)\n{\n\n}\n\n"
-                         "void " name "::init()\n{\n\n}\n\n"
-                         "void " name "::reset()\n{\n\n}\n\n"
-                         name "& " name "::operator=(const " name "& src)\n{\nreturn (*this);\n}\n"))
-	(beginning-of-buffer)
-	(while (and (not (eobp)) (forward-line))
-	  (indent-according-to-mode))
-	(beginning-of-buffer)
-	(search-forward "Description:")))
+  (defun my-flymake-toggle ()
+    (if (not (member 'flymake-mode-on c-mode-common-hook))
+        (progn
+          ;; Enable this keybinding (my-flymake-minor-mode) by default
+          ;; Added by Hartmut 2011-07-05
+          (add-hook 'c-mode-common-hook 'my-flymake-minor-mode)
+          (add-hook 'c-mode-common-hook 'flymake-mode-on)
+          (add-hook 'c-mode-common-hook 'my-flymake-google-init)
+          (flymake-mode-on)
+          (message "flymake-mode Enabled"))
+      (remove-hook 'c-mode-common-hook 'my-flymake-minor-mode)
+      (remove-hook 'c-mode-common-hook 'flymake-mode-on)
+      (remove-hook 'c-mode-common-hook 'my-flymake-google-init)
+      (flymake-mode-off)
+      (message "flymake-mode Disabled"))))
 
 
 ;; @see https://stackoverflow.com/questions/7299893/getting-rid-of-buffer-has-running-process-confirmation-when-the-process-is-a-f
